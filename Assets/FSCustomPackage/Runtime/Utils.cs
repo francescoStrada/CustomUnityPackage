45312231e824dc2d8aad12e91d474a5929f87d2b
Francesco Strada <francesco.strada11@gmail.com>
2021-11-20 18:17:24 +0100

    custom package setup

---
+++ Assets/FSCustomPackage/Runtime/Utils.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CustomPackage
+{
+    public static class Utils
+    {
+        public static void SampleFunction()
+        {
+            Debug.Log("A very stupid function");
+        }
+    }
+}
